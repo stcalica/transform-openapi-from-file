bc659380ae470cf5dc8056aa1f66a095f52f901d
Kyle <kyle@Mac.attlocal.net>
2025-01-12 00:52:51 -0800

    add support for file output in GitHub Action to resolve output length limitation Fixes #1

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const core = require('@actions/core');
 const fs = require('fs');
-var Converter = require('openapi-to-postmanv2');
+const path = require('path');
+const Converter = require('openapi-to-postmanv2');
 
 async function run() {
     try {
         const schemaPath = core.getInput('openapi_schema_path');
         const openapiSchema = fs.readFileSync(schemaPath, 'utf8');
 
+        const outputFilePath = core.getInput('output_file') || './postman-collection.json';
+
         Converter.convert({ type: 'string', data: openapiSchema }, {}, (err, result) => {
             if (!result.result) {
                 core.setFailed(`Conversion failed: ${result.reason}`);
             } else {
-                if (outputFilePath) {
-                    fs.writeFileSync(outputFilePath, JSON.stringify(postmanCollection, null, 2), 'utf8');
-                    console.log(`Postman collection written to ${outputFilePath}`);
-                    core.setOutput('postman_collection', outputFilePath);
-                } else {
-                    core.setOutput('postman_collection', JSON.stringify(postmanCollection, null, 2));
-                }
+                const postmanCollection = result.output[0].data;
+                const resolvedPath = path.resolve(outputFilePath);
+                fs.writeFileSync(resolvedPath, JSON.stringify(postmanCollection, null, 2), 'utf8');
+                console.log(`Postman collection written to ${resolvedPath}`);
+                core.setOutput('postman_collection_file', resolvedPath);
             }
         });
     } catch (error) {
